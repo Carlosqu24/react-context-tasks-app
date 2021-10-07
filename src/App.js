6839570213137511a65e0e8f1f38e9e106d1be6c
Carlos Quesada <carlosquesadaumana@gmail.com>
2021-10-07 10:19:14 -0600

    HashRouter added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 // PROVIDER
 import { ContextProvider } from './context/GlobalContext';
 
