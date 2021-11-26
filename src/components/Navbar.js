ac27e3197a73d52f1fc1276adad0fdb98f49e8b5
Carlos Quesada <carlosquesadaumana@gmail.com>
2021-11-25 23:28:30 -0600

    Navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
       return (
-            <nav className="navbar navbar-expand-lg">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                   <div className="container">
                         <Link to="/" className="navbar-logo">
                               <h2>TASK APP</h2>
@@ -14,8 +14,8 @@ export const Navbar = () => {
                         </button>
                         <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                               <div className="navbar-nav">
-                                    <Link to="/"  className="navbar-link mx-3">Home</Link>
-                                    <Link to="/create" className="navbar-link mx-3">Create Note</Link>
+                                    <Link to="/"  className="navbar-link m-3">Home</Link>
+                                    <Link to="/create" className="navbar-link m-3">Create Note</Link>
                               </div>
                         </div>
                   </div>
